ea2919fbfe5dde9c7885a9947a80271ae7819ebe
MarceloLima11 <marcelolima152021@gmail.com>
2021-12-09 09:32:23 -0300

    Configuring IlhaController for IlhaRepository

--- Controllers/IlhaController.cs
+++ Controllers/IlhaController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WikiPiece.Data;
 using WikiPiece.Models;
+using WikiPiece.Repository.Interfaces;
 
 namespace WikiPiece.Controllers
 {
@@ -11,9 +12,9 @@ namespace WikiPiece.Controllers
     [Route("v1/[Controller]")]
     public class IlhaController : ControllerBase
     {
-        private readonly WikiPieceContext _context;
+        private readonly IIlhaRepository _context;
 
-        public IlhaController(WikiPieceContext context)
+        public IlhaController(IIlhaRepository context)
         {
             _context = context;
         }
@@ -21,46 +22,43 @@ namespace WikiPiece.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<Ilha>> GetAll()
         {
-            var ilhas = _context.Ilhas.ToList();
+            var ilhas = _context.Get().ToList();
             return ilhas;
         }   
 
         [HttpGet("{id}")]
         public ActionResult<Ilha> GetById([FromRoute] int id)
         {
-            var ilha = _context.Ilhas.FirstOrDefault(x => x.Id == id);
+            var ilha = _context.GetById(x => x.Id == id);
             return ilha;
         }
 
         [HttpGet("Regiao")]
         public ActionResult<IEnumerable<Ilha>> GetByRegiao(string regiao)
         {
-            var ilhasRegiao =_context.Ilhas.Where(x => x.Regiao == regiao).ToList();
+            var ilhasRegiao =_context.GetByRegiao(x => x.Regiao == regiao).ToList();
             return ilhasRegiao;
         }
 
         [HttpGet("Clima")]
         public ActionResult<IEnumerable<Ilha>> GetByClima(string clima)
         {
-            var ilhasClima = _context.Ilhas.Where(x => x.Clima == clima).ToList();
+            var ilhasClima = _context.GetByClima(x => x.Clima == clima).ToList();
             return ilhasClima;
         }
 
         [HttpPost]
         public ActionResult<Ilha> Post([FromBody] Ilha ilha)
         {
-            _context.Ilhas.Add(ilha);
-            _context.SaveChanges();
+            _context.Add(ilha);
             return ilha;
         }
 
         [HttpPut("{id}")]
         public IActionResult Put([FromRoute] int id, [FromBody] Ilha newIlha)
         {
-            var oldIlha = _context.Ilhas.FirstOrDefault(x => x.Id == id);
-            _context.Entry(oldIlha).State = EntityState.Modified;
             _context.Update(newIlha);
-
+            
             return NoContent();
         }
     }
